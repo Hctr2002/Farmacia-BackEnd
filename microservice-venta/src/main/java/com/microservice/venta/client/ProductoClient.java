7b2b2bd4ab3bfb72cb8343882e2b00ce193a313b
Héctor <hectorrobledo2332@gmail.com>
2025-09-22 12:12:53 -0300

    Solucionado error al agregar venta

--- microservice-venta/src/main/java/com/microservice/venta/client/ProductoClient.java
+++ microservice-venta/src/main/java/com/microservice/venta/client/ProductoClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "msvc-producto", path = "http://localhost:8003/api/v1/productos")
+@FeignClient(name = "msvc-producto", path = "/api/v1/productos")
 public interface ProductoClient {
 
     @GetMapping("/{id}")
